1c7321a0975def51d26ec4d320d5535c305efd7e
Ian Walter <public@iankwalter.com>
2019-03-26 21:14:02 -0400

    Adding eslint

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { h, render } from 'preact'
 
-render((
-	<div id="foo">
-		<span>Hello, world!</span>
-		<button onClick={ e => alert("hi!") }>Click Me</button>
-	</div>
-), document.getElementById('app'))
+render(
+  (
+    <div id="foo">
+      <span>Hello, world!</span>
+      <button onClick={e => alert('hi!')}>Click Me</button>
+    </div>
+  ),
+  document.getElementById('app')
+)
